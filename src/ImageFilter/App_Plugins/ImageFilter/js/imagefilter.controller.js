c7dea89d34d8c7f890ba8f79bcac0e8b02150d74
Paul Seal <prjseal@gmail.com>
2019-01-14 00:23:57 +0000

    - initial commit

---
+++ src/ImageFilter/App_Plugins/ImageFilter/js/imagefilter.controller.js
@@ -0,0 +1,15 @@
+﻿angular.module("umbraco")
+    .controller("ImageFilter", function ($scope, editorState) {
+
+        var mediaUrl = "";
+
+        var vm = this;
+
+        if (editorState.current.contentType.alias === "Image") {
+            mediaUrl = editorState.current.mediaLink;
+            vm.mediaUrl = mediaUrl;
+        }
+
+        console.log(mediaUrl);
+
+    });
