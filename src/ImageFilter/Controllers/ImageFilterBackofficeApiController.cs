eadf1aa28f268214a87226e2b3a13bb3f1fc3d86
Paul Seal <paul@moriyama.co.uk>
2019-01-15 00:24:38 +0000

    - setting up the project to work with app plugins from imagefilter project and copy them into the imagefilter.web project after build

---
+++ src/ImageFilter/Controllers/ImageFilterBackofficeApiController.cs
@@ -0,0 +1,16 @@
+﻿using Umbraco.Web.Editors;
+using Umbraco.Web.Mvc;
+
+namespace ImageFilter.Controllers
+{
+    [PluginController("ImageFilter")]
+    public class ImageFilterBackofficeApiController : UmbracoAuthorizedJsonController
+    {
+        public string CreateImageWithFilterOption()
+        {
+            //just setting up dummy api method at the moment
+            string url = "/umbraco#/media/media/edit/1061";
+            return url;
+        }
+    }
+}
